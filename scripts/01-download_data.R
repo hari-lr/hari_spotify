10e29738b076a47044384ff368b3652a868a7827
hari-lr <156157882+hari-lr@users.noreply.github.com>
2024-02-29 09:47:58 -0500

    Add files via upload

---
+++ scripts/01-download_data.R
@@ -0,0 +1,29 @@
+#### Preamble ####
+# Purpose: Downloads and saves the data from NASA API
+# Author: Hari Lee
+# Date: 29 February 2023
+# Contact: hari.leerobledo@mail.utoronto.ca
+# License: MIT
+# Pre-requisites: -
+# Any other information needed: -
+
+
+#### Workspace setup ####
+library(httr)
+library(tidyverse)
+
+
+#### Download data ####
+NASA <- 
+  GET("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2024-02-28")
+
+content(NASA)
+
+content(NASA)$explanation
+
+knitr::include_graphics(content(NASA)$url)
+
+
+#### Save data ####
+
+         
