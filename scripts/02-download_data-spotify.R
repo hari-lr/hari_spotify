c3e6331d3a1b961b8e280114e9609772ebd496e3
hari-lr <156157882+hari-lr@users.noreply.github.com>
2024-02-29 10:25:15 -0500

    Add files via upload

---
+++ scripts/02-download_data-spotify.R
@@ -0,0 +1,23 @@
+#### Preamble ####
+# Purpose: Downloads and saves the data from the Spotify API
+# Author: Hari Lee
+# Date: 29 February 2024
+# Contact: hari.leerobledo@mail.utoronto.ca
+# License: MIT
+# Pre-requisites: -
+# Any other information needed: -
+
+
+#### Workspace setup ####
+library(tidyverse)
+library(spotifyr)
+
+
+#### Download data ####
+beyonce <- get_artist_audio_features("beyonce")
+
+saveRDS(beyonce, "beyonce.rds")
+
+#### Save data ####
+
+         
